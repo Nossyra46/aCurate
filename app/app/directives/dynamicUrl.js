bf7e54be81c49e218b683dbe7c3b90c9fd71a9a1
Nossyra46 <nossyra@gmail.com>
2016-10-23 12:44:42 +0200

    videos

---
+++ app/app/directives/dynamicUrl.js
@@ -0,0 +1,8 @@
+myApp.directive('dynamicUrl', function () {
+    return {
+        restrict: 'A',
+        link: function postLink(scope, element, attr) {
+            element.attr('src', attr.dynamicUrlSrc);
+        }
+    };
+});
--- server.js
+++ server.js
@@ -3,9 +3,16 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Statically serving Angular application
 app.use(express.static(__dirname + "/app"));
+
+app.use('/api', require(__dirname + '/app/routes/api.js'));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + "/app/views/index.html"));
